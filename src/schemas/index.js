046fd7c3fa46d16675665064567fa1322b0f9835
Christian Smith <smith@anvil.io>
2016-11-13 06:10:16 -0800

    feat(schemas): index module

---
+++ src/schemas/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  AccessTokenSchema: require('./AccessTokenSchema'),
+  AuthorizationCodeSchema: require('./AuthorizationCodeSchema'),
+  ClientSchema: require('./ClientSchema'),
+  IDTokenSchema: require('./IDTokenSchema'),
+  ProviderSchema: require('./ProviderSchema'),
+  UserSchema: require('./UserSchema')
+}
