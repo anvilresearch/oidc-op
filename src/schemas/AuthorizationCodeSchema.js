6d1d518c7d09524f2585cbd6c75c90871f887fa6
Christian Smith <smith@anvil.io>
2016-11-13 06:09:35 -0800

    feat(AuthorizationCodeSchema): schema definition

---
+++ src/schemas/AuthorizationCodeSchema.js
@@ -0,0 +1,58 @@
+/**
+ * Dependencies
+ */
+const {JSONSchema} = require('json-document')
+
+/**
+ * AuthorizationCodeSchema
+ */
+const AuthorizationCodeSchema = new JSONSchema({
+  properties: {
+    code: {
+      type: 'string'
+    },
+
+    sub: {
+      type: 'string'
+    },
+
+    aud: {
+      type: 'string'
+    },
+
+    redirect_uri: {
+      type: 'string',
+      format: 'uri'
+    },
+
+    exp: {
+      type: 'integer'
+    },
+
+    max: {
+      type: 'integer'
+    },
+
+    scope: {
+      type: 'string'
+    },
+
+    nonce: {
+      type: 'string'
+    }
+
+    // expires_at
+    // client_id
+    // redirect_uri
+    // max_age
+    // user_id
+    // scope
+    // used
+    // nonce
+  }
+})
+
+/**
+ * Export
+ */
+module.exports = AuthorizationCodeSchema
