6ee0b97168acee930ca28d222eb0e61b293f61c5
Christian Smith <smith@anvil.io>
2016-11-15 06:15:36 -0800

    refactor(IDToken): issue method to DRY auth and token endpoints

--- src/IDToken.js
+++ src/IDToken.js
@@ -15,6 +15,48 @@ class IDToken extends JWT {
   static get schema () {
     return IDTokenSchema
   }
+
+  /**
+   * issue
+   */
+  static issue (request, response) {
+    let {params, code, provider, client, subject} = request
+    let {issuer, keys, backend} = provider
+
+    let alg = client['id_token_signed_response_alg'] || 'RS256'
+    let key = keys['id_token'].signing[alg].privateKey
+    let kid = keys['id_token'].signing[alg].publicJwk.kid
+    let iss = issuer
+    let jti = request.random()
+    let iat = Math.floor(Date.now() / 1000)
+    let aud, sub, max, nonce
+
+    // authentication request
+    if (!code) {
+      aud = client['client_id']
+      sub = subject['_id']
+      //max = parseInt(params['max_age']) || client['default_max_age'] || 3600
+      nonce = params.nonce
+
+    // token request
+    } else {
+      aud = code.aud
+      sub = code.sub
+      //max = parseInt(code['max']) || client['default_max_age'] || 3600
+      nonce = code.nonce
+    }
+
+    let exp = iat + max
+
+    let header = {alg, kid}
+    let payload = {iss, aud, sub, exp, iat, jti, nonce}
+    let jwt = new IDToken({header, payload, key})
+
+    return jwt.encode().then(compact => {
+      response['id_token'] = compact
+      return response
+    })
+  }
 }
 
 /**
