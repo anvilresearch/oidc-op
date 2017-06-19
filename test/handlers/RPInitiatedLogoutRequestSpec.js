6c4581113f3178fc92a53ec6b794f14e0e158691
Dmitri Zagidulin <dzagidulin@gmail.com>
2017-06-19 13:08:33 -0400

    test(RPInitiatedLogoutRequest): Convert test to use dirty-chai

--- test/handlers/RPInitiatedLogoutRequestSpec.js
+++ test/handlers/RPInitiatedLogoutRequestSpec.js
@@ -4,6 +4,7 @@ const chai = require('chai')
 const expect = chai.expect
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
+chai.use(require('dirty-chai'))
 chai.use(sinonChai)
 chai.should()
 const HttpMocks = require('node-mocks-http')
@@ -31,11 +32,11 @@ describe('RPInitiatedLogoutRequest', () => {
   describe('handle()', () => {
     it('should invoke injected host.logout', done => {
       let res = HttpMocks.createResponse()
-      let logoutSpy = sinon.stub(provider.host, 'logout', () => Promise.resolve())
+      let logoutSpy = sinon.stub(provider.host, 'logout').resolves()
 
       RPInitiatedLogoutRequest.handle(reqNoParams, res, provider)
         .then(() => {
-          expect(logoutSpy).to.have.been.called
+          expect(logoutSpy).to.have.been.called()
           done()
         })
     })
@@ -72,7 +73,7 @@ describe('RPInitiatedLogoutRequest', () => {
 
       request.redirectOrRespond()
 
-      expect(request.respond).to.not.have.been.called
+      expect(request.respond).to.not.have.been.called()
       expect(res.statusCode).to.equal(302)
       expect(res._getRedirectUrl()).to.equal(postLogoutRedirectUri)
       done()
@@ -85,7 +86,7 @@ describe('RPInitiatedLogoutRequest', () => {
 
       request.redirectOrRespond()
 
-      expect(request.redirectToRP).to.not.have.been.called
+      expect(request.redirectToRP).to.not.have.been.called()
       expect(res.statusCode).to.equal(204)
       done()
     })
